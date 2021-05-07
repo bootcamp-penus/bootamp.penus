23e3b684e80e647f1c9fda02231f81fa82384cbe
bootcamp-penus <83744087+bootcamp-penus@users.noreply.github.com>
2021-05-07 16:04:33 +0700

    Add files via upload

---
+++ if.php
@@ -0,0 +1,21 @@
+?<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+}
+    
+    $nilai = 90;
+        
+    if(nilai < 85){
+        echo "nilai anda 0";
+    }else if ($nilai <= 60){
+        echo "nilai anda C";
+    }else{
+        echo "nilai anda A";
+</body>
+</html>
---
+++ operator.php
@@ -0,0 +1,27 @@
+?<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=</pjhp<">
+    <title>Document</title>
+
+    <php?
+    $a= 2;
+    $b= 3;
+
+
+    echo $c = $a + $b; //5
+    echo "<br>";
+    echo $d = $a - $b; // -1
+    echo "<br>";
+    echo $e = $a * $b; // 6
+    echo "<br>";
+    echo $f = $a / $b // 0.667
+    echo "<br>";
+    echo $g = $b % $a // 1
+
+    $angka1 = 2;
+        
+</body>
+</html>
